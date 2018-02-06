e728b3c5d626fd688ece8353c25843353b79ac1a
Lauritz Valved <lauritzv@gmail.com>
2018-02-06 13:15:44 +0100

    droppet program.cs

--- Solsystem/MainWindow.xaml.cs
+++ Solsystem/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SpaceSim;
+using System.Reflection;
+using System.IO;
+
 
 namespace Solsystem
 {
@@ -23,6 +27,69 @@ namespace Solsystem
         public MainWindow()
         {
             InitializeComponent();
+
+            List<SpaceObject> solarSystem = initSolarSystem();
+
+
         }
+        public List<SpaceObject> initSolarSystem()
+        {
+            string path = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"SpaceObjects.txt");
+            string[] lines = System.IO.File.ReadAllLines(path);
+
+            string[][] jaggedArray = lines.Select(line => line.Split(' ').ToArray()).ToArray();
+
+            List<SpaceObject> solarSystem = new List<SpaceObject>();
+
+            foreach (string[] line in jaggedArray)
+            {
+                string obj = line[0];
+                string name = line[1];
+                int orbRad = Convert.ToInt32(line[2]);
+                int orbPeriod = Convert.ToInt32(line[3]);
+                int objRad = Convert.ToInt32(line[4]);
+                double rotPeriod = Convert.ToDouble(line[5]);
+                string color = line[6];
+
+                switch (obj)
+                {
+                    case "Star":
+                        solarSystem.Add(new Star(name, orbRad, orbPeriod, objRad, rotPeriod, color));
+                        break;
+                    case "Planet":
+                        solarSystem.Add(new Planet(name, orbRad, orbPeriod, objRad, rotPeriod, color));
+                        break;
+                    case "Moon":
+                        solarSystem.Add(new Moon(name, orbRad, orbPeriod, objRad, rotPeriod, color));
+                        break;
+
+                }
+
+            }
+
+            foreach (SpaceObject obj in solarSystem)
+            {
+                switch (obj.Name)
+                {
+                    case "Sun":
+                        break;
+                    case "The Moon":
+                        obj.Parent = solarSystem[3];
+                        break;
+                    default:
+                        obj.Parent = solarSystem[0];
+                        break;
+                }
+
+            }
+
+
+            return solarSystem;
+        }
+
+
     }
+
 }
+
+    
